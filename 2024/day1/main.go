d2be8afa69b68e1b60c47cbb1fde110ee44bd423
Julie Qiu <julie@golang.org>
2024-12-01 22:40:28 -0500

    2024/day1

---
+++ 2024/day1/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+const input = "inputs.txt"
+
+func main() {
+	list1, list2, err := createLists()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := problem1(list1, list2); err != nil {
+		log.Fatal(err)
+	}
+	if err := problem2(list1, list2); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func problem1(list1, list2 []int) (err error) {
+	var score int
+	for i := range list1 {
+		a := list1[i]
+		b := list2[i]
+		c := a - b
+		if c < 0 {
+			c = c * -1
+		}
+		score += c
+	}
+	fmt.Printf("Answer (part 1): %d\n", score)
+	return nil
+}
+
+func problem2(list1, list2 []int) (err error) {
+	var score int
+	for i := range list1 {
+		for j := range list2 {
+			if list1[i] == list2[j] {
+				score += list1[i]
+			}
+		}
+	}
+	fmt.Printf("Answer (part 2): %d\n", score)
+	return nil
+}
+
+func createLists() (_ []int, _ []int, err error) {
+	file, err := os.Open(input)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer func() {
+		cerr := file.Close()
+		if err == nil {
+			err = cerr
+		}
+	}()
+
+	lists := make([][]int, 2)
+	scanner := bufio.NewScanner(file)
+	// optionally, resize scanner's capacity for lines over 64K, see next example
+	for scanner.Scan() {
+		parts := strings.Fields(scanner.Text())
+		for i, n := range parts {
+			num, err := strconv.Atoi(n)
+			if err != nil {
+				return nil, nil, err
+			}
+			lists[i] = append(lists[i], num)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, nil, err
+	}
+
+	if len(lists[0]) != len(lists[1]) {
+		return nil, nil, fmt.Errorf("lists[0] = %d; lists[1] = %d", len(lists[0]), lists[1])
+	}
+
+	sort.Sort(sort.IntSlice(lists[0]))
+	sort.Sort(sort.IntSlice(lists[1]))
+	return lists[0], lists[1], nil
+}
