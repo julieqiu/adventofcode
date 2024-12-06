4a543681bb6befbcf4d77614307855ed0e42bd81
Julie Qiu <julie@golang.org>
2024-12-05 21:41:46 -0500

    2024/template: add

---
+++ 2024/template/main_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/julieqiu/adventofcode/2024/internal/runner"
+)
+
+func TestProblem1(t *testing.T) {
+	runner.RunTest(t, problem1, 0)
+}
+
+func TestProblem2(t *testing.T) {
+	runner.RunTest(t, problem2, 0)
+}
