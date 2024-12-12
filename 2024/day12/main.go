185f57211f11d56efb25f738e840a60cf0d2a350
Julie Qiu <julie@golang.org>
2024-12-12 09:59:14 -0500

    2024/day12

---
+++ 2024/day12/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+
+	"github.com/julieqiu/adventofcode/2024/internal/runner"
+)
+
+func main() {
+	if err := runner.Run(problem1, problem2); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func problem1(lines []string) (int, error) {
+	var count int
+	return count, nil
+}
+
+func problem2(lines []string) (int, error) {
+	var count int
+	return count, nil
+}
